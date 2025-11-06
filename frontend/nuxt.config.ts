787cc097920fae8bd3ec3ef747e0a14a8ad42476
mpdev-id <mnprasetya@gmail.com>
2025-11-06 16:07:03 +0700

    nuxt configuration ok

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,27 +1,12 @@
-import tailwindcssVite from '@tailwindcss/vite';
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['/../app/assets/css/tailwind.css'],
-
-  postcss: {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  },
 
   runtimeConfig: {
     public: {
-      apiBase: 'http://localhost:8000/api',
+      apiBase: 'http://127.0.0.1:8000/api',
     },
   },
 
-  vite: {
-    plugins: [tailwindcssVite()],
-  },
-
-  modules: ['@nuxt/icon'],
+  modules: [],
 })
