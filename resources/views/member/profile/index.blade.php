2b509bac6f5a8991810d8a5dc94818ed8e418eef
mpdev <mnprasetya@gmail.com>
2025-11-24 01:34:59 +0700

    profile for admin

--- resources/views/member/profile/index.blade.php
+++ resources/views/member/profile/index.blade.php
@@ -38,7 +38,13 @@
         </div>
     </div>
 
-    <div class="mt-6">
+    <div class="mt-6 space-y-3">
+        <template x-if="user.role && user.role.includes('Super Admin')">
+            <a href="{{ route('admin.dashboard.index') }}" class="btn btn-primary btn-outline w-full gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" /></svg>
+                Admin Dashboard
+            </a>
+        </template>
         <button @click="logout" class="btn btn-error btn-outline w-full gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" /></svg>
             Logout
