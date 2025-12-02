37f1918a5752ee5860bb1f51406c157b0195863c
mpdev-id <mnprasetya@gmail.com>
2025-12-02 07:31:02 +0700

    issues approver logic and admin logout button

--- resources/views/template/member.blade.php
+++ resources/views/template/member.blade.php
@@ -68,7 +68,7 @@
                 <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                     <li><a href="{{ route('dashboard-member') }}">Dashboard</a></li>
                     <li><a href="{{ route('member.leaves.index') }}">My Leaves</a></li>
-                    <template x-if="userRole !== 'Member'">
+                    <template x-if="userRole !== 'Employee'">
                         <li><a href="{{ route('member.approver-log.index') }}">Approver Log</a></li>
                     </template>
                     <li><a href="{{ route('member.profile.index') }}">Profile</a></li>
@@ -93,7 +93,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" /></svg>
             <span class="text-[10px] font-medium">Leaves</span>
         </a>
-        <template x-if="userRole !== 'Member'">
+        <template x-if="userRole !== 'Employee'">
             <a href="{{ route('member.approver-log.index') }}" class="{{ request()->routeIs('member.approver-log.*') ? 'active text-primary bg-primary/10' : 'text-base-content/60 hover:text-primary' }} flex-1 flex flex-col items-center justify-center gap-1 h-full transition-colors duration-200">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span class="text-[10px] font-medium">Approvals</span>
